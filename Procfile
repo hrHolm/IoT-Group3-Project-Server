99dc87b346efd91eb9779df3b07623e779c4192e
Frederik Holm <hollm@live.dk>
2020-05-01 13:51:01 +0200

    init django project

---
+++ Procfile
@@ -0,0 +1,2 @@
+release: python manage.py migrate
+web: gunicorn lightheartedbulb.wsgi --log-file -
---
+++ dashboard/models.py
@@ -0,0 +1,15 @@
+from django.db import models
+
+# Create your models here.
+class Device(models.Model):
+    device_id = models.IntegerField(primary_key=True,)
+    
+class LightData(models.Model):
+    device = models.ForeignKey(Device, on_delete=models.CASCADE)
+    timestamp = models.FloatField()
+    value = models.FloatField()
+
+class SetpointData(models.Model):
+    device = models.ForeignKey(Device, on_delete=models.CASCADE)
+    timestamp = models.FloatField()
+    value = models.FloatField()
